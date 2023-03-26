72c8f41cd31c94861852dff33f1da93d5ad1fddd
ryosuke-horie <ryosuke03140@gmial.com>
2023-03-26 13:44:11 +0900

    MySQLとの接続を解決しローカルで表示

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
+      port: 3306,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
