5c00f47a945a9b198445b9f76636aef3cf57d49f
ryosuke-horie <ryosuke03140@gmial.com>
2023-03-30 00:37:22 +0900

    dotenvを開発時のみ参照するように変更

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+// 開発環境のみdotenvを読み込む
 import * as dotenv from 'dotenv';
-dotenv.config();
-
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 @Module({
   imports: [
     TypeOrmModule.forRoot({
